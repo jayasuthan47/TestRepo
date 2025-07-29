7a597ff3bcc2286772b78be27221b7bc52895b60
Jayasuthan Jayaraj <jayasuthan_jayaraj@trimble.com>
2025-07-29 15:42:57 +0530

    new file added

---
+++ execute.js
@@ -0,0 +1,23 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+
+
+function execute(a, b, angle) {
+    return a + b + angle;
+}
+
+console.log(execute(a, b, c, d, e, f, g, h, i, j));
+
+execute(a, b);
+
+
+
